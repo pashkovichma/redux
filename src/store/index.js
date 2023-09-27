9925312ec14c58371e776eb0422ec7ed860de7b5
Maria <pashkovichma@gmail.com>
2023-09-27 23:31:01 +0200

    dispatching actions from inside components

--- src/store/index.js
+++ src/store/index.js
@@ -1,21 +1,21 @@
-import { legacy_createStore as createStore} from 'redux'
- 
-const counterReducer = (state = { counter: 0 }, action) => {
-  if (action.type === 'decrement') {
-    return {
-      counter: state.counter - 1,
-    };
-  }
+import { createStore } from 'redux';
 
+const counterReducer = (state = { counter: 0 }, action) => {
   if (action.type === 'increment') {
     return {
       counter: state.counter + 1,
     };
   }
 
-  return state;
-}
+  if (action.type === 'decrement') {
+    return {
+      counter: state.counter - 1,
+    };
+  }
 
-const store = createStore();
+  return state;
+};
+
+const store = createStore(counterReducer);
 
 export default store;
